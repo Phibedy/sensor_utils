a02d064b4f6e822a5d4cd440c6b72163699a9627
Hans Kirchner <hans-kirchner@gmx.de>
2015-06-01 14:24:04 +0200

    Add DynamicEntity

---
+++ src/dynamic_entity.cpp
@@ -0,0 +1,26 @@
+#include "sensor_utils/dynamic_entity.h"
+
+DynamicEntity::DynamicEntity() : position(0, 0), viewDirection(1, 0),
+    velocity(0), moveDirection(1, 0), lastPositon(0, 0), lastVelocity(0) {}
+
+void DynamicEntity::updatePosition(const lms::math::vertex2f &position,
+                                   const lms::math::vertex2f &viewDirection) {
+    this->lastPositon = this->position;
+    this->position = position;
+    this->viewDirection = viewDirection;
+}
+
+void DynamicEntity::updateVelocity(float velocity,
+                    const lms::math::vertex2f &moveDirection) {
+    this->lastVelocity = this->velocity;
+    this->velocity = velocity;
+    this->moveDirection = moveDirection;
+}
+
+float DynamicEntity::movedDistance() const {
+    return this->position.distance(this->lastPositon);
+}
+
+float DynamicEntity::deltaVelocity() const {
+    return this->velocity - this->lastVelocity;
+}
