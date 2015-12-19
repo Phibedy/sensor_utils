39169e814c7efaa1e68fece1929154410857cce0
Markus Herb <mail@markusherb.de>
2015-12-19 18:41:37 +0100

    Refactor IMU, add covariances

--- include/sensor_utils/imu.h
+++ include/sensor_utils/imu.h
@@ -2,17 +2,28 @@
 #define SENSOR_UTILS_IMU_H
 
 #include "sensor.h"
-namespace sensor_utils{
-struct Vector{
-    float x;
-    float y;
-    float z;
-};
+#include "types.h"
 
-struct IMU: public Sensor{
-    Vector acc;
-    Vector gyro;
-    Vector magnetometer;
+namespace sensor_utils
+{
+
+struct IMU : public Sensor{
+    typedef Vector3 Measurement;
+    typedef Matrix3 Covariance;
+
+    //! Linear acceleration measurement along x,y,z-axes [g]
+    Measurement accelerometer;
+    //! Angular velocity measurement around x,y,z-axes [rad/s]
+    Measurement gyroscope;
+    //! Magnetic field strength along x,y,z-axes [T]
+    Measurement magnetometer;
+
+    //! Covariance of acceleration measurements
+    Covariance accelerometerCovariance;
+    //! Covariance of angular velocity measurements
+    Covariance gyroscopeCovariance;
+    //! Covariance of magnetic field measurements
+    Covariance magnetometerCovariance;
 };
 }
 
